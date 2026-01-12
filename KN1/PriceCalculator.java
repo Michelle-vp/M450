0162554c414aeca5156e6f87644fa2bfd524b8ef
Michelle-vp <150661566+Michelle-vp@users.noreply.github.com>
2026-01-12 02:02:58 +0100

    Update PriceCalculator.java

--- KN1/PriceCalculator.java
+++ KN1/PriceCalculator.java
@@ -1,1 +1,27 @@
+public class PriceCalculator {
 
+    public static double calculatePrice(
+            double baseprice,
+            double specialprice,
+            double extraprice,
+            int extras,
+            double discount
+    ) {
+        double addonDiscount;
+
+        if (extras >= 5) {
+            addonDiscount = 15.0;
+        } else if (extras >= 3) {
+            addonDiscount = 10.0;
+        } else {
+            addonDiscount = 0.0;
+        }
+
+        double result =
+                baseprice / 100.0 * (100.0 - discount)
+                        + specialprice
+                        + extraprice / 100.0 * (100.0 - addonDiscount);
+
+        return result;
+    }
+}
